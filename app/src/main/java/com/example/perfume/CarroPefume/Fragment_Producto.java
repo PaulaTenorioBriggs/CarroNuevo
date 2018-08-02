b0c99bf4c7f7d54458fcf5c9eadc8beaeff21bcc
paula tenorio <kari.962012@hotmail.com>
2018-08-01 20:18:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/perfume/CarroPefume/Fragment_Producto.java
@@ -0,0 +1,19 @@
+package com.example.perfume.CarroPefume;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.perfume.CarroPefume.R;
+
+
+public class Fragment_Producto extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment__producto, container, false);
+    }
+}
